929c3d76bb0e8a2903a3fd3ca45331d44a3df0cb
eljesam <124920260+eljesam@users.noreply.github.com>
2024-03-13 12:43:04 +0000

    Add files via upload

---
+++ CA5_OOP/src/main/java/org/example/BookStore.java
@@ -0,0 +1,14 @@
+package org.example;
+import sql.*;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class BookStore {
+    public static void main(String[] args) {
+         Books book = new Books;
+         book.start();
+
+    }
+
+}
