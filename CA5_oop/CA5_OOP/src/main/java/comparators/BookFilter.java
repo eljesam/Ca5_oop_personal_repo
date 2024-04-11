0743ae3d3979e81ab880aadf4082cdc7177da982
eljesam <124920260+eljesam@users.noreply.github.com>
2024-04-11 14:40:16 +0100

    Add files via upload
    Updated the project with all the changes made in the shared repo

---
+++ CA5_oop/CA5_OOP/src/main/java/comparators/BookFilter.java
@@ -0,0 +1,11 @@
+package comparators;
+import DTOs.Book;
+import java.util.Comparator;
+
+public class BookFilter implements Comparator<Book> {
+    @Override
+    public int compare(Book o1, Book o2) {
+        return o1.getTitle().compareTo(o2.getTitle());
+    }
+}
+
