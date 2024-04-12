af347e08a58f73dde1566a74f7ad703a14b60554
eljesam <124920260+eljesam@users.noreply.github.com>
2024-04-12 09:39:11 +0100

    created test file to test all methods

---
+++ CA5_oop/src/test/java/org/example/BookStoreTest.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookStoreTest {
+
+        @org.junit.jupiter.api.Test
+        void main() {
+        }
+
+        @org.junit.jupiter.api.Test
+        void findAllBooks() {
+
+        }
+
+        @org.junit.jupiter.api.Test
+        void getBookByID() {
+        }
+
+        @org.junit.jupiter.api.Test
+        void deleteBookByID() {
+        }
+
+        @org.junit.jupiter.api.Test
+        void insertBook() {
+        }
+
+        @org.junit.jupiter.api.Test
+        void updateBookByID() {
+        }
+
+        @org.junit.jupiter.api.Test
+        void findBookByFilter() {
+        }
+
+}
